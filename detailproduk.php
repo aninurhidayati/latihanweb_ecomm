1b3efd5bbbd6eac3ada78c05b9d1ed80b483e8b2
Wans20 <wans200102@gmail.com>
2022-06-30 22:00:21 +0700

    new login

--- detailproduk.php
+++ detailproduk.php
@@ -1,10 +1,6 @@
-<?php
-				$dtlproduk = mysqli_query($koneksidb,"select m.*, k.nmproduk from mst_produk m inner join kategoriproduk k
-					on m.id_kategori=k.id_kategori order by id_produk DESC")
-					or die("gagal akses table mst_menu ".mysqli_error($koneksidb));
-				//looping 
-				while($r = mysqli_fetch_array($dtlproduk)){	
-			?>
+<?php 
+
+?>
 <div class="container pb-5">
 	<div class="row">
 		<div class="col-md-3 pt-4">
@@ -12,22 +8,26 @@
 			<div class="subkategori" id="subkategori"></div>
 		</div>
 		<div class="col-md-9 pt-4">
+		<?php
+				$dtlproduk = mysqli_query($koneksidb,"select*from mst_produk ")
+					or die("gagal akses table mst_menu ".mysqli_error($koneksidb));
+				//looping 
+				while($p = mysqli_fetch_array($dtlproduk)){	
+					if(isset($_GET['id']) == ($_GET['id']==$p['idproduk'])){
+			?>
 			<div class="row">
 				<div class="col-md-6 pe-0">
-					<img src="assets/img/<?= $b['gambar'];?>" class="card-img-top" alt="..." />
+					<img src="assets/img/<?= $p['gambar'];?>" class="card-img-top" alt="..." />
 				</div>
 				<div class="col-md-6 ps-0">
 					<div class="card">
 						<div class="card-body subkategori p-2">
-							<h4><?= $b['nmproduk'];?></h4>
+							<h4><?= $p['nmproduk'];?></h4>
 							<h5 class="harga">Rp. 100,000</h5>
 							<p style="color: black; font-family: Arial, Helvetica, sans-serif; font-size: 14px">
 								Kondisi : Baru <br />
 								Berat : 450Gram <br />
 								Ukuran : Allsize <br />
-								Sweatshirt saat ini merupakan salah satu lini pakaian terbaik dan berkualitas tinggi di
-								antara Local Brand Indonesia. Dengan
-								model loose-fit berlengan panjang tanpa tudung.
 							</p>
 						</div>
 						<ul class="list-group list-group-flush">
@@ -41,5 +41,8 @@
 			</div>
 		</div>
 	</div>
-	<?php }?>
+	<?php
+	}
+	}
+	?>
 </div>

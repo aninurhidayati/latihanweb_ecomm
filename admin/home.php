44540aefd4bf90a7dfbbb2b736953fde250ceaad
ani <aninur.h@gmail.com>
2022-06-30 10:06:35 +0700

    ani

--- admin/home.php
+++ admin/home.php
@@ -13,30 +13,34 @@ security_login();
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Halaman Admin </title>
 	<!-- link bootstrap -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<!-- link icon bootstrap -->
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
-	<script src="https://cdn.tiny.cloud/1/ctai2l7ettpdz3uyphr0lz4x23v2z3otpascq7sk3miw64e3/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+	<script src="https://cdn.tiny.cloud/1/ctai2l7ettpdz3uyphr0lz4x23v2z3otpascq7sk3miw64e3/tinymce/6/tinymce.min.js"
+		referrerpolicy="origin"></script>
 	<script>
-      tinymce.init({
-        selector: 'textarea#deskripsi',
-        plugins: [
-          'a11ychecker','advlist','advcode','advtable','autolink','checklist','export',
-          'lists','link','image','charmap','preview','anchor','searchreplace','visualblocks',
-          'powerpaste','fullscreen','formatpainter','insertdatetime','media','table','help','wordcount'
-        ],
-        toolbar: 'undo redo | formatpainter casechange blocks | bold italic backcolor | ' +
-          'alignleft aligncenter alignright alignjustify | ' +
-          'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help'
-      });
-    </script>
+	tinymce.init({
+		selector: 'textarea#deskripsi',
+		plugins: [
+			'a11ychecker', 'advlist', 'advcode', 'advtable', 'autolink', 'checklist', 'export',
+			'lists', 'link', 'image', 'charmap', 'preview', 'anchor', 'searchreplace', 'visualblocks',
+			'powerpaste', 'fullscreen', 'formatpainter', 'insertdatetime', 'media', 'table', 'help',
+			'wordcount'
+		],
+		toolbar: 'undo redo | formatpainter casechange blocks | bold italic backcolor | ' +
+			'alignleft aligncenter alignright alignjustify | ' +
+			'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help'
+	});
+	</script>
 
 	<style>
-		body {
-			padding-top: 60px;
-		}
+	body {
+		padding-top: 60px;
+	}
 	</style>
 </head>
+
 <body>
 	<nav class="navbar fixed-top navbar-light bg-primary">
 		<div class="container-fluid">
@@ -69,6 +73,9 @@ security_login();
 					if (isset($_GET['modul'])) {
 						include_once $_GET['modul'] . "/index.php";
 					}
+					else{
+						include_once "dashboard.php";
+					}
 					?>
 				</div>
 			</div>
@@ -76,7 +83,8 @@ security_login();
 	</section>
 
 	<!-- Letakkan script JS disini -->
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
 	</script>
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 	<script src="../assets/proses.js"></script>
@@ -87,4 +95,5 @@ security_login();
 	<script src="../assets/js/agung.js"></script>
 	<script src="../assets/js/putra.js"></script>
 </body>
+
 </html>
